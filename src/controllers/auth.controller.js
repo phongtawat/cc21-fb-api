debbd95f7ce36a9977666ed3935cac894fc8784c
phongtawat <mr.phongtawat@gmail.com>
2025-10-28 14:07:05 +0700

    first commit

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,18 @@
+// export function register(req, res) {
+//  req.send('Register Controller')
+// }
+
+export const register = (req, res) => {
+    res.send('Register Controller')
+}
+
+export function login(req,res) {
+   res.json({
+   msg : 'Login Controller',
+   body : req.body
+ })
+}
+
+export const getMe = (req,res) => {
+ res.json({msg : 'GetMe controller'})
+}
